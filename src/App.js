2331bd2c6a65baf20d1c82cd0a127a5c89fbb365
dLukachev <a89266384045@gmail.com>
2025-04-28 14:45:39 +0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
 
 function App() {
+  const [user, setUser] = useState(null);
+
+
+  useEffect(() => {
+    const tg = window.Telegram.WebApp;
+    tg.ready();
+    setUser(tg.initDataUnsafe?.user);
+    tg.expand();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Mini App</h1>
+      {user ? <p>Привет, {user.first.name}!</p> : <p>Загрузка...</p>}
     </div>
   );
 }
