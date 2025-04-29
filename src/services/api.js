b31390396096827935dc84317980cbb2735325ed
Данил <ddrxg@icloud.com>
2025-04-29 14:53:41 +0300

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: 'http://127.0.0.1:3333',
+  baseURL: 'https://backend-rest-hrya.onrender.com',
   headers: {
     'Content-Type': 'application/json',
   },
